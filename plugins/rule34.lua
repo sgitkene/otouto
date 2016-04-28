0e4bded4f7ca106b482b7248556d1ed9aa2cfffd
sgitkene <max.erb@hotmail.com>
2016-04-28 17:09:10 +0000

    added dogify, fixed rule34

--- plugins/rule34.lua
+++ plugins/rule34.lua
@@ -14,18 +14,46 @@ local triggers = {
 local action = function(msg)
 	local input = msg.text:input()
 	local output = 'nothing'
+	local answer = ''
+	local status = 0
+	local base_url = 'http://rule34.paheal.net/post/list/'
+	local randomint = 0
 	if not input then
 		output = doc
 	else
+		input = input:gsub(' ', '_')
 		input = URL.escape(input)
 		print(input)
-		answer, status = HTTP.request('http://supertwins.nl/old/r34.py?img='..input)
+		answer, status = HTTP.request(base_url..input..'/1')
 		if status ~= 200 then
-			output = config.errors.connection
+			output = config.errors.connection..'1'
 		else
-			answer = answer:match('http.*%....')
-			print(answer)
-			output = '[rule34!]('..answer..')'
+			maxnum = answer:match('<a href=".-(%d*)">Last</a>')
+			if maxnum then
+				maxnum = tonumber(maxnum)
+				maxnum = math.random(maxnum)
+			else
+				maxnum = 1
+			end
+			print(maxnum)
+			answer, status = HTTP.request(base_url..input..'/'..randomint)
+			if status ~= 200 then
+				output = config.errors.connection..'2'
+			else
+				local temptable = {}
+				numbert = 1
+				for imgname in answer:gmatch('<a href="(.-)">Image Only</a>') do
+					temptable[numbert] = imgname
+					numbert = numbert +1
+				end
+				randomint = math.random(#temptable)
+				answer = temptable[randomint]
+				if not answer then
+					output = config.errors.results
+				else
+					output = '[rule34!]('..answer..')'
+				end
+			end
 		end
 	end
 	
