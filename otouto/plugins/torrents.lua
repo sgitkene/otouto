3f9f48e8244e20423c511f07fbdad7f23d8353b6
sgitkene <max.erb@hotmail.com>
2016-06-18 20:54:24 +0000

    added torrents plugin, returns magnet links to a requested torrent.

--- otouto/plugins/torrents.lua
+++ otouto/plugins/torrents.lua
@@ -1,44 +1,58 @@
-local commant = 'kat (query)'
-local doc = [[```
-/kat (query)
+local torrents = {}
+
+utilities = require('otouto.utilities')
+HTTPS = require('ssl.https')
+URL = require('socket.url')
+JSON = require('dkjson')
+
+torrents.command = 'torrents (query)'
+torrents.doc = [[```
+/torrents (query)
 Returns torrent search results.
+Alias: /tse
 ```]]
 
-local triggers = {
-	'^/kat[@'..bot.username..']*'
-}
+function torrents:init(config)
+	torrents.triggers = utilities.triggers(self.info.username, config.cmd_pat):t('torrents', true):t('tse',true).table
+end
+torrents.api = 'https://torrentproject.se/?s='
+torrents.options = '&out=json'
+function torrents:action(msg, config)
 
-local action = function(msg)
-
-	local input = msg.text:input()
+	local input = utilities.input(msg.text)
 	if not input then
-		mesg.reply_to_message and msg.reply_to_message.text then
+		if msg.reply_to_message and msg.reply_to_message.text then
 			input = msg.reply_to_message.text
 		else
-			sendMessage(msg.chat.id, doc, true, msg.message_id, true)
+			utilities.send_message(self, msg.chat.id, doc, true, msg.message_id, true)
 			return
 		end
 	end
 
+	local url = torrents.api .. URL.escape(input) ..torrents.options
+
 	local jstr, res = HTTPS.request(url)
 	if res ~= 200 then
-		sendReply(msg, config.errors.connection)
+		utilities.send_reply(self, msg, config.errors.connection)
 		return
 	end
 
 	local jdat = JSON.decode(jstr)
-	if jdat.pageInfo.totalResults == 0 then
-		sendReply(msg, config.errors.results)
+	if jdat.total_found == 0 then
+		utilities.send_reply(self, msg, config.errors.results)
 		return
 	end
 
---fill in actual
+	local output = ''
+
+	for i = 1, math.min(tonumber(jdat.total_found), 5) do
+		local cur = jdat[tostring(i)] 
+		local magnet = 'magnet:?xt=urn:btih:'
+		output = output .. cur.title .. ' s:' .. cur.seeds .. ' l:' .. cur.leechs .. ' ' .. cur.torrent_size .. 'bytes \n' .. magnet .. cur.torrent_hash .. '\n'
+	end
+
+	utilities.send_message(self, msg.chat.id, output, true, nil, false)
 
 end
 
-return {
-	action = action,
-	triggers = triggers,
-	doc = doc,
-	command = command
-}
+return torrents
